26145201ea2c50037005c4ad236e51b068080f61
Rudransh Khare <kharerudransh6@gmail.com>
2026-07-30 00:36:46 +0530

    Use VITE_API_URL for production

--- FRONTEND/src/features/auth/services/auth.api.js
+++ FRONTEND/src/features/auth/services/auth.api.js
@@ -1,8 +1,33 @@
+// import axios from "axios";
+
+// const api = axios.create({
+//     baseURL: "http://localhost:3000/api/auth",
+//     withCredentials: true
+// });
+
+// export async function register({ name, username, email, password }) {
+//     const response = await api.post("/register", { name, username, email, password });
+//     return response.data;
+// }
+
+// export async function login({ username, email, password }) {
+//     const response = await api.post("/login", { username, email, password });
+//     return response.data;
+// }
+// export async function getMe() {
+//     const response = await api.get("/get-Me");
+//     return response.data;
+// }
+// export async function logout() {
+//     const response = await api.get("/logout");
+//     return response.data;
+// }
+
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:3000/api/auth",
-    withCredentials: true
+    baseURL: `${import.meta.env.VITE_API_URL}/api/auth`,
+    withCredentials: true,
 });
 
 export async function register({ name, username, email, password }) {
@@ -14,10 +39,12 @@ export async function login({ username, email, password }) {
     const response = await api.post("/login", { username, email, password });
     return response.data;
 }
+
 export async function getMe() {
     const response = await api.get("/get-Me");
     return response.data;
 }
+
 export async function logout() {
     const response = await api.get("/logout");
     return response.data;
