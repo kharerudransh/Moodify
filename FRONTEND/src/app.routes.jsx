4cf9dd436cff57578c04238fdd5061007c1a22ba
Rudransh Khare <kharerudransh6@gmail.com>
2026-07-30 00:24:55 +0530

    Fix case-sensitive imports

--- FRONTEND/src/app.routes.jsx
+++ FRONTEND/src/app.routes.jsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter } from "react-router";
-import Register from "./features/auth/pages/register";
-import Login from "./features/auth/pages/login";
+import Register from "./features/auth/pages/Register";
+import Login from "./features/auth/pages/Login";
 import Protected from "./features/auth/component/Protected";
 import FaceExpression from "./features/expression/component/FaceExpression";
 import Layout from "./features/shared/component/Layout";
